24068ee0895ef86c92850020ecd6281391eeaa4f
Vince <vincentjg01@gmail.com>
2026-07-16 19:57:16 -0400

    Functional test now checks we can input a to-do item

--- lists/tests.py
+++ lists/tests.py
@@ -4,9 +4,10 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
+        response = self.client.get('/')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
+
+
+

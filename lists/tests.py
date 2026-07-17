b90dd7e63147634c1a18817f650938174a920e92
Vince <vincentjg01@gmail.com>
2026-07-16 20:45:20 -0400

    Refractor hompeage view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -5,9 +5,16 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
+        self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "</html>")
+        self.assertTemplateUsed(response, 'home.html')  
+
+    def test_renders_homepage_content(self):
+        response = self.client.get('/')
+        self.assertContains(response, "To-Do")
+
+        
 
 
 

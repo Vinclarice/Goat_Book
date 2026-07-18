3a27dc4406b147e8bba24de2fbb21a1baf484943
Vince <vincentjg01@gmail.com>
2026-07-18 19:21:17 -0400

    Haack FT runner to be able to test docker

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 import time
+import os
 
 MAX_WAIT = 5
 
@@ -13,6 +14,8 @@ MAX_WAIT = 5
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        if test_server := os.environ.get("TEST_SERVER"):
+            self.live_server_url = "http://" + test_server
 
     def tearDown(self):
         self.browser.quit()
